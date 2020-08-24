13c3b77d7c6cb7fbe84659d9f041bdfda3b39cd2
Kazuki Ota <kaota@microsoft.com>
2020-08-25 00:15:28 +0900

    Add project files.

---
+++ BindingEnums/EnumBindingSourceExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace BindingEnums
+{
+    public class EnumBindingSourceExtension : MarkupExtension
+    {
+        private readonly Type _enumType;
+
+        public EnumBindingSourceExtension(Type enumType)
+        {
+            if (!enumType.IsEnum)
+            {
+                throw new ArgumentException($"{enumType} is not enum.");
+            }
+
+            _enumType = enumType ?? throw new ArgumentNullException(nameof(enumType));
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => 
+            Enum.GetValues(_enumType);
+    }
+}
